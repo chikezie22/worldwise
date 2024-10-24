55cc6fc44e950619a1a3071165633a66a954eac0
chikezie22 <simonchikezie44@gmail.com>
2024-10-24 14:40:09 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+function NotFound() {
+  return (
+    <div>
+      <PageNav />
+      Not found
+    </div>
+  );
+}
+
+export default NotFound;

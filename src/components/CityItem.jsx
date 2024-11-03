b851ea890d014733c73da2e9910d1f02f0694a1c
chikezie22 <simonchikezie44@gmail.com>
2024-11-03 15:15:56 +0100

    added search Params

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -30,11 +30,17 @@ const formatDate = (date) =>
     // weekday: "short",
   }).format(new Date(date));
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+  const {
+    cityName,
+    emoji,
+    date,
+    id,
+    position: { lat, lng },
+  } = city;
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}`}>
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      Map
+      <h1>
+        Positions Lat={lat} Lng={lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 20, lng: 50 })}>
+        Change Position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
